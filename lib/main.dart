323ff9d04ace7500550b84ff81d136341df24141
Nathan Furman <ncfusn@gmail.com>
2020-09-26 23:40:07 +0300

    Scaffold added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-/*
 void main() {
   runApp(AppCore());
-} */
+}
 
-void main() => AppCore(); // shorter way to implement entry point
+//void main() => AppCore(); // shorter way to implement entry point
 
 class AppCore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Some Title'),
+        ),
+        body: Text('Some other texts'),
+      ),
     );
   }
 }
