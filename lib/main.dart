8190cbfae5afb706dbb3bb7dc5ac180d6ac197cb
Nathan Furman <ncfusn@gmail.com>
2020-09-26 22:59:34 +0300

    Main Widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
+/*
+void main() {
+  runApp(AppCore());
+} */
+
+void main() => AppCore(); // shorter way to implement entry point
+
+class AppCore extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
