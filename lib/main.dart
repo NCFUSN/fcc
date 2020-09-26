4790167ab2b007038a9cb2352d39ebebd48f1918
Nathan Furman <ncfusn@gmail.com>
2020-09-27 00:22:36 +0300

    named and anonimous functions wired to buttons

--- lib/main.dart
+++ lib/main.dart
@@ -8,15 +8,30 @@ import 'package:flutter/cupertino.dart';
 void main() => runApp(AppCore()); // shorter way to implement entry point
 
 class AppCore extends StatelessWidget {
+  void answerQuestion() {
+    print('answering');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Some Title'),
-        ),
-        body: Text('Some other texts'),
-      ),
+          appBar: AppBar(
+            title: Text('Some Title'),
+          ),
+          body: Column(
+            children: [
+              Text('The Question'),
+              RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+              RaisedButton(
+                  child: Text('Answer 2'), onPressed: () => print('answering')),
+              RaisedButton(
+                  child: Text('Answer 3'),
+                  onPressed: () {
+                    print('answering');
+                  })
+            ],
+          )),
     );
   }
 }
