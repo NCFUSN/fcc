2a016a4ff36c9a9e5652bb6c92c251979f261d09
Nathan Furman <ncfusn@gmail.com>
2020-10-01 22:10:40 +0300

    Show quiz results

--- lib/main.dart
+++ lib/main.dart
@@ -18,22 +18,42 @@ class AppCore extends StatefulWidget {
 
 class _AppState extends State<AppCore> {
   var _questionIndex = 0;
+  var _totalScore = 0;
   final _questions = const [
     {
       'questionText': "What is your favourite colour?",
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 5},
+        {'text': 'Red', 'score': 7},
+        {'text': 'Green', 'score': 9},
+        {'text': 'White', 'score': 10}
+      ],
     },
     {
       'questionText': "What's your favourite animal?",
-      'answers': ['Cat', 'Dog', 'Rat', 'Frog'],
+      'answers': [
+        {'text': 'Cat', 'score': 10},
+        {'text': 'Dog', 'score': 7},
+        {'text': 'Rat', 'score': 5},
+        {'text': 'Frog', 'score': 4}
+      ],
     },
     {
       'questionText': "Who is your favourite person?",
-      'answers': ['Lady Diana', 'Mosley', 'Churchill', 'Thatcher'],
+      'answers': [
+        {'text': 'Lady Diana', 'score': 9},
+        {'text': 'Churchill', 'score': 7},
+        {'text': 'Thatcher', 'score': 10},
+        {'text': 'Mosley', 'score': 4}
+      ]
     },
   ];
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    print(_totalScore);
+
     if (_questionIndex < _questions.length) {
       print(null.hashCode);
     }
@@ -46,16 +66,18 @@ class _AppState extends State<AppCore> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Questionary'),
-          ),
-          body: _questionIndex < _questions.length
-              ? Quiz(
-                  questions: _questions,
-                  answerQuestion: _answerQuestion,
-                  questionIndex: _questionIndex)
-              : Result()),
-    );
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Questionary'),
+      ),
+      body: _questionIndex < _questions.length
+          ? Quiz(
+              questions: _questions,
+              answerQuestion: _answerQuestion,
+              questionIndex: _questionIndex)
+          : Result(
+              resultScore: _totalScore,
+            ),
+    ));
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result({@required this.resultScore});
+  String get resultPhrase {
+    var resultText = 'You did it!';
+    if (resultScore >= 12) {
+      resultText = 'You are a good person';
+    } else {
+      resultText = 'You are a bloody maniac';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
